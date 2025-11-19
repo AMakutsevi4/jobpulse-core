2e15749d158c9df973605a3328944133af041b96
AMakutsevi4 <amakutsevich@bk.ru>
2025-11-19 21:07:18 +1000

    [week-0] add structure project

---
+++ src/main/java/ru/jobpulse/jobpulse_core/JobPulseCoreApplication.java
@@ -0,0 +1,13 @@
+package ru.jobpulse.jobpulse_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPulseCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPulseCoreApplication.class, args);
+	}
+
+}
