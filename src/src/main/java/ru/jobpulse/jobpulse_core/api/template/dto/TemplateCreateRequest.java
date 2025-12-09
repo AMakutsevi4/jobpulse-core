0c83e52ad85bb19105e4045df015d639a44d5e58
Alexander Makutsevich <82426375+AMakutsevi4@users.noreply.github.com>
2025-12-09 22:16:40 +1000

    Refactor/clean structure (#7)
    * fix: clean-structure
    
    * feat: add db
    
    * feat: add db

--- src/src/main/java/ru/jobpulse/jobpulse_core/api/template/dto/TemplateCreateRequest.java
+++
@@ -1,7 +0,0 @@
-package ru.jobpulse.jobpulse_core.api.template.dto;
-
-public record TemplateCreateRequest(
-        String title,
-        String content
-) {
-}
--- src/src/main/java/ru/jobpulse/jobpulse_core/application/template/dto/TemplateUpdateRequest.java
+++
@@ -1,7 +0,0 @@
-package ru.jobpulse.jobpulse_core.application.template.dto;
-
-public record TemplateUpdateRequest(
-        String title,
-        String content
-) {
-}
