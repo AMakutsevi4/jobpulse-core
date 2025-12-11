e0d2549badcf31ed85226a03a393e926c00a2d11
Alexander Makutsevich <82426375+AMakutsevi4@users.noreply.github.com>
2025-12-12 09:05:19 +1000

    [week 3] add prod profile and update log (#10)

--- src/main/java/ru/jobpulse/core/infrastructure/client/HhVacancySearchRepository.java
+++ src/main/java/ru/jobpulse/core/infrastructure/client/HhVacancySearchRepository.java
@@ -1,18 +1,18 @@
 package ru.jobpulse.core.infrastructure.client;
 
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 import ru.jobpulse.core.domain.model.VacancyCount;
 import ru.jobpulse.core.domain.repository.VacancySearchRepository;
 
 @Repository
+@Profile("prod")
+@RequiredArgsConstructor
 public class HhVacancySearchRepository implements VacancySearchRepository {
 
     private final HhApiClient client;
-
-    public HhVacancySearchRepository(HhApiClient client) {
-        this.client = client;
-    }
 
     @Override
     public VacancyCount findVacancyCount(String query) {
